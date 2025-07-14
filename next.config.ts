a856a7cff1d1660558b54f726df571a6d28397f1
Lucas Czulak <lucas.czulak@buddy.works>
2025-07-14 13:55:17 +0200

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
   images: {
     unoptimized: true
   }
